f22034ccdb79338786ca0929ce80e4b3545f09ad
eddjuacor <eddjuarez1209@outlook.es>
2024-03-25 14:07:44 -0600

    actualizaciones

--- src/components/main/coments/Coments.jsx
+++ src/components/main/coments/Coments.jsx
@@ -1,13 +1,16 @@
 import { Target } from "./Target"
 import { Target01 } from "./Target01"
 import { Target02 } from "./Target02"
+import { Target03 } from "./Target03"
 
 export const Coments = () => {
   return (
     <div className="md:w-[100%]  mx-auto flex flex-wrap justify-center gap-2 mt-10">
+        <Target03/>
         <Target/>
         <Target01/>
         <Target02/>
+        
     </div>
   )
 }
