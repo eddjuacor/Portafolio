cc45a0f4ba233d46c384aef2310ba81df23fcf12
eddjuacor <eddjuarez1209@outlook.es>
2024-03-25 11:04:15 -0600

    actualizaciones

--- src/components/header/Info.jsx
+++ src/components/header/Info.jsx
@@ -7,8 +7,8 @@ export const Info = () => {
     <div className="flex  w-full pt-4">
       <div className="flex gap-2">
         <a href="" className=" no-underline font-mono text-slate-400 hover:text-slate-600"><img src={cv} alt="" className=" size-6 md:size-7"/></a>
-        <a href="" className="no-underline font-mono text-slate-400 hover:text-slate-600">https://www.linkedin.com/in/eddju/<img src={linkedin} alt="" className=" size-6 md:size-7"/></a>
-        <a href="" className="no-underline font-mono text-slate-400 hover:text-slate-600">https://github.com/eddjuacor<img src={github} alt="" className=" size-6 md:size-7" /></a>
+        <a href="https://www.linkedin.com/in/eddju/" className="no-underline font-mono text-slate-400 hover:text-slate-600"><img src={linkedin} alt="" className=" size-6 md:size-7"/></a>
+        <a href="https://github.com/eddjuacor" className="no-underline font-mono text-slate-400 hover:text-slate-600"><img src={github} alt="" className=" size-6 md:size-7" /></a>
       </div>
     </div>
   )
